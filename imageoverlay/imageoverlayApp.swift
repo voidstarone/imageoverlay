a08738ad92f452e78eea8a8356bcce8a6cf4dbc0
Thomas Lee <thomas.lee@boohoo.com>
2022-04-05 20:51:11 +0100

    Initial Commit

---
+++ imageoverlay/imageoverlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  imageoverlayApp.swift
+//  imageoverlay
+//
+//  Created by Thomas Lee on 05/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct imageoverlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
